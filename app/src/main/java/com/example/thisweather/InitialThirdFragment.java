340a9ac8e4df6cc177a83b0af0ef8e0ad4944844
ChoiAhHyun <cah1113@naver.com>
2019-02-21 18:58:26 +0900

    초기화 액티비티

---
+++ app/src/main/java/com/example/thisweather/InitialThirdFragment.java
@@ -0,0 +1,63 @@
+package com.example.thisweather;
+
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+import com.dpro.widgets.WeekdaysPicker;
+
+public class InitialThirdFragment  extends Fragment {
+    TextView AmPm;
+    TextView Time;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_initial_third, null);
+
+        LinearLayout setTime = (LinearLayout) view.findViewById(R.id.ll_time);
+        AmPm = view.findViewById(R.id.tv_ampm);
+        Time = view.findViewById(R.id.tv_time);
+
+        setTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), AlertDialog.THEME_HOLO_LIGHT, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+                        setTV(hourOfDay, minute);
+                    }
+                },8,30,false);
+                timePickerDialog.show();
+            }
+        });
+
+        WeekdaysPicker weekdaysPicker = (WeekdaysPicker) view.findViewById(R.id.wp_weekdays);
+        Typeface type = Typeface.createFromAsset(getActivity().getAssets(),"res/font/sf_pro_text_semibold.ttf");
+//        weekdaysPicker.setT
+
+        return view;
+    }
+
+    private void setTV(int hourOfDay, int minute) {
+        if (hourOfDay <= 12){
+            AmPm.setText("오전");
+            Time.setText(hourOfDay + "시 " + minute + "분");
+        }
+        else {
+            AmPm.setText("오후");
+            int newTime = hourOfDay - 12;
+            Time.setText(newTime + "시 " + minute + "분");
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/thisweather/TabPagerAdapter.java
@@ -0,0 +1,39 @@
+package com.example.thisweather;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+
+    // Count number of tabs
+    private int tabCount = 3;
+
+    public TabPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        // Returning the current tabs
+        switch (position) {
+            case 0:
+                InitialFirstFragment tabFragment1 = new InitialFirstFragment();
+                return tabFragment1;
+            case 1:
+                InitialSecondFragment tabFragment2 = new InitialSecondFragment();
+                return tabFragment2;
+            case 2:
+                InitialThirdFragment tabFragment3 = new InitialThirdFragment();
+                return tabFragment3;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
